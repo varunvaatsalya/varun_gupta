0d064a77c1fe47682ad19e6376c3e08a83ea839b
varunvaatsalya <a.varungupta23@gmail.com>
2024-05-18 01:01:41 +0530

    comma bug fix

--- src/app/AboutContent.js
+++ src/app/AboutContent.js
@@ -26,13 +26,13 @@ function AboutContent() {
             <div className="text-sky-400">
               self<span className="text-white">.</span>location{" "}
               <span className="text-white"> = </span>
-              <span className="text-orange-700">"Lucknow, India"</span>
+              <span className="text-orange-700">&#34;Lucknow, India&#34;</span>
             </div>
             <div className="text-sky-400">
               self<span className="text-white">.</span>passion{" "}
               <span className="text-white"> = </span>
               <span className="text-orange-700">
-                "Dedicated to bringing the future of web development to life"
+                &#34;Dedicated to bringing the future of web development to life&#34;
               </span>
             </div>
             <div className="text-sky-400">
@@ -42,15 +42,15 @@ function AboutContent() {
             </div>
             <div className="pl-8 border-l border-white">
               <div className="text-orange-700">
-                "Crafting cutting-edge backend systems"
+                &#34;Crafting cutting-edge backend systems&#34;
                 <span className="text-white">,</span>
               </div>
               <div className="text-orange-700">
-                "Developing robust, scalable, and optimized web applications"
+                &#34;Developing robust, scalable, and optimized web applications&#34;
                 <span className="text-white">,</span>
               </div>
               <div className="text-orange-700">
-                "Ensuring top-tier performance and reliability"
+                &#34;Ensuring top-tier performance and reliability&#34;
               </div>
             </div>
             <div className="text-yellow-500">]</div>
@@ -58,15 +58,15 @@ function AboutContent() {
               self<span className="text-white">.</span>mission{" "}
               <span className="text-white"> = </span>
               <span className="text-orange-700">
-                "Join me on a journey to build the digital backbone that powers
-                tomorrow's most innovative experiences."
+                &#34;Join me on a journey to build the digital backbone that powers
+                tomorrow's most innovative experiences.&#34;
               </span>
             </div>
             <div className="text-sky-400">
               self<span className="text-white">.</span>learning{" "}
               <span className="text-white"> = </span>
               <span className="text-orange-700">
-                "Machine Learning Development"
+                &#34;Machine Learning Development&#34;
               </span>
             </div>
           </div>
@@ -85,13 +85,13 @@ function AboutContent() {
               <span className="text-yellow-500">(</span>
             </div>
             <div className="pl-8 border-l border-white">
-              <div className=""><span className="text-blue-500">f</span><span className="text-orange-700">"Location: </span><span className="text-pink-500">&#123;<span className="text-sky-400">self<span className="text-white">.</span>location</span>&#125;</span><span className="text-blue-500">\n</span><span className="text-orange-700">"</span></div>
-              <div className=""><span className="text-blue-500">f</span><span className="text-orange-700">"Passion: </span><span className="text-pink-500">&#123;<span className="text-sky-400">self<span className="text-white">.</span>passion</span>&#125;</span><span className="text-blue-500">\n</span><span className="text-orange-700">"</span></div>
+              <div className=""><span className="text-blue-500">f</span><span className="text-orange-700">&#34;Location: </span><span className="text-pink-500">&#123;<span className="text-sky-400">self<span className="text-white">.</span>location</span>&#125;</span><span className="text-blue-500">\n</span><span className="text-orange-700">&#34;</span></div>
+              <div className=""><span className="text-blue-500">f</span><span className="text-orange-700">&#34;Passion: </span><span className="text-pink-500">&#123;<span className="text-sky-400">self<span className="text-white">.</span>passion</span>&#125;</span><span className="text-blue-500">\n</span><span className="text-orange-700">&#34;</span></div>
               <div className="text-white">
-              <span className="text-orange-700">"Misson:<span className="text-blue-500">\n</span>  - " <span className="text-white"> + </span>"<span className="text-blue-500">\n</span> - "</span><span> .</span><span className="text-yellow-200">join</span><span className="text-pink-500">(<span className="text-sky-400">self<span className="text-white">.</span>expertise</span>)</span> + <span className="text-orange-700">"<span className="text-blue-500">\n</span>"</span>
+              <span className="text-orange-700">&#34;Misson:<span className="text-blue-500">\n</span>  - &#34; <span className="text-white"> + </span>&#34;<span className="text-blue-500">\n</span> - &#34;</span><span> .</span><span className="text-yellow-200">join</span><span className="text-pink-500">(<span className="text-sky-400">self<span className="text-white">.</span>expertise</span>)</span> + <span className="text-orange-700">&#34;<span className="text-blue-500">\n</span>&#34;</span>
               </div>
-              <div className=""><span className="text-blue-500">f</span><span className="text-orange-700">"Misson: </span><span className="text-pink-500">&#123;<span className="text-sky-400">self<span className="text-white">.</span>mission</span>&#125;</span><span className="text-blue-500">\n</span><span className="text-orange-700">"</span></div>
-              <div className=""><span className="text-blue-500">f</span><span className="text-orange-700">"Curently Learning: </span><span className="text-pink-500">&#123;<span className="text-sky-400">self<span className="text-white">.</span>learning</span>&#125;</span><span className="text-blue-500">\n</span><span className="text-orange-700">"</span></div>
+              <div className=""><span className="text-blue-500">f</span><span className="text-orange-700">&#34;Misson: </span><span className="text-pink-500">&#123;<span className="text-sky-400">self<span className="text-white">.</span>mission</span>&#125;</span><span className="text-blue-500">\n</span><span className="text-orange-700">&#34;</span></div>
+              <div className=""><span className="text-blue-500">f</span><span className="text-orange-700">&#34;Curently Learning: </span><span className="text-pink-500">&#123;<span className="text-sky-400">self<span className="text-white">.</span>learning</span>&#125;</span><span className="text-blue-500">\n</span><span className="text-orange-700">&#34;</span></div>
             </div>
             <div className="text-yellow-500">)</div>
             <div className="">
@@ -102,7 +102,7 @@ function AboutContent() {
           </div>
           </div>
         </div>
-        <div className="mt-4 text-white"><span className="text-pink-500">if </span><span className="text-sky-400">__name__ </span>  == <span className="text-orange-700">"__main__"</span>:</div>
+        <div className="mt-4 text-white"><span className="text-pink-500">if </span><span className="text-sky-400">__name__ </span>  == <span className="text-orange-700">&#34;__main__&#34;</span>:</div>
         <div className="pl-8 border-l border-white text-white">
           <div ><span className="text-sky-400">freelancer</span> = <span className="text-emerald-500">Freelancer</span><span className="text-yellow-500">()</span></div>
           <div ><span className="text-sky-400">freelancer</span>.<span className="text-yellow-200">display_profile</span><span className="text-yellow-500">()</span></div>
