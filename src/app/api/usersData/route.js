a4ea956873b86d10529a9f3aefe5f6eee36683c6
varunvaatsalya <a.varungupta23@gmail.com>
2024-10-24 14:38:20 +0530

    permission

--- src/app/api/usersData/route.js
+++ src/app/api/usersData/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export function GET(Request){
-    return NextResponse.json({data:false},{status:200})
+    return NextResponse.json({data:true},{status:200})
 } 
