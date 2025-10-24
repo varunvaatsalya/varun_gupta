f76791c86144baf1afb50526ee423d288563104c
varunvaatsalya <a.varungupta23@gmail.com>
2025-10-25 00:16:05 +0530

    sticky components

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,11 @@ import CoMain from "./CoMain";
 export default function Home() {
   return (
     <>
-      <div className={"max-w-screen-lg max-md:mx-auto flex gap-3 my-1 mx-auto"}>
+      <div className={"max-w-screen-lg max-md:mx-auto flex items-start gap-3 my-1 mx-auto"}>
         <div className="w-[98%] lg:w-3/4 mx-auto">
           <Main />
         </div>
-        <div className="my-2 w-1/4 max-lg:hidden">
+        <div className="sticky top-2 my-2 w-1/4 max-lg:hidden">
           <CoMain />
         </div>
       </div>
