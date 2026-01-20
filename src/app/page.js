90b22ab6add8f13b357649d8ed8ac454f6b429e0
varunvaatsalya <a.varungupta23@gmail.com>
2026-01-20 13:31:33 +0530

    maintainence

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,30 @@
 import Main from "./Main";
 import CoMain from "./CoMain";
 
-
-
 export default function Home() {
   return (
     <>
-      <div className={"max-w-screen-lg max-md:mx-auto flex items-start gap-3 my-1 mx-auto"}>
-        <div className="w-[98%] lg:w-3/4 mx-auto">
+      <div
+        className={
+          "max-w-screen-lg max-md:mx-auto flex items-start gap-3 my-1 mx-auto"
+        }
+      >
+        <h1 className="text-3xl font-bold text-gray-800 mb-2">
+          🚧 Site Under Maintenance
+        </h1>
+        <p className="text-gray-600">
+          Thodi der ke liye site band hai. Hum jaldi wapas aayenge.
+        </p>
+        {/* <div className="w-[98%] lg:w-3/4 mx-auto">
           <Main />
         </div>
         <div className="sticky top-2 my-2 w-1/4 max-lg:hidden">
           <CoMain />
-        </div>
+        </div> */}
       </div>
     </>
   );
 }
-
 
 export function generateMetadata({ params }) {
   return {
@@ -26,5 +33,3 @@ export function generateMetadata({ params }) {
       "This Portfolio website is owned by Varun Gupta who is a Backend Developer",
   };
 }
-
-
