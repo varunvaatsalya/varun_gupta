90af46c38e6cb95a0147e926aaddf76bd31201fc
varunvaatsalya <a.varungupta23@gmail.com>
2026-01-20 13:44:55 +0530

    on

--- src/app/page.js
+++ src/app/page.js
@@ -9,18 +9,12 @@ export default function Home() {
           "max-w-screen-lg max-md:mx-auto flex items-start gap-3 my-1 mx-auto"
         }
       >
-        <h1 className="text-3xl font-bold text-gray-800 mb-2">
-          🚧 Site Under Maintenance
-        </h1>
-        <p className="text-gray-600">
-          Thodi der ke liye site band hai. Hum jaldi wapas aayenge.
-        </p>
-        {/* <div className="w-[98%] lg:w-3/4 mx-auto">
+        <div className="w-[98%] lg:w-3/4 mx-auto">
           <Main />
         </div>
         <div className="sticky top-2 my-2 w-1/4 max-lg:hidden">
           <CoMain />
-        </div> */}
+        </div>
       </div>
     </>
   );
